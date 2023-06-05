b58b44c0299b54c680364d0fd39ec9a7ca619d0b
Alecpu92 <alessandro_belfiore@hotmail.it>
2023-06-05 18:14:07 +0200

    aggiunta vari import export + aggiunta componenti

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,15 @@
-<script setup>
+<script>
 import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios';
+
+export default {
+  component: {
+    HelloWorld
+  }
+}
 </script>
+
+
 
 <template>
   <div>
@@ -15,18 +24,5 @@ import HelloWorld from './components/HelloWorld.vue'
 </template>
 
 <style lang="scss">
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+@use './styles/general.scss'
 </style>
